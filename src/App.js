ef4aeeb289c777d08b3cdd6754fe0e6a0af3d0a6
lakshmi-unni <lakshmiunnioffcial@gmail.com>
2024-12-07 17:32:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Intro from "./components/Intro/Intro";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/image.png";
+import bg1 from '../../assets/image1.png'
+import { Link } from "react-scroll";
+import btnImg from "../../assets/hireme.png";
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm<span className="introName"> Lakshmi</span>
+          <br />
+          Website Designer
+        </span>
+        <p className="introPara">
+            
+          I am a skilled web designer with experience in creating <br/>visually
+          appealing and user friendly websites
+        </p>
+        <Link>
+          <button className="btn">
+            <img className="btnImg" src={btnImg} alt="Hire Me"/>
+              Hire Me
+            
+          </button>
+        </Link>
+      </div>
+      {/* <img src={bg} alt="profile" className="bg" /> */}
+      <img src={bg} alt="profile" className="bg" />
+    </section>
+  );
+};
+
+export default Intro;
