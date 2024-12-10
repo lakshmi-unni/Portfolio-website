df7b3e6f11612830a205509c4a10d245ef1fd1ed
lakshmi-unni <lakshmiunnioffcial@gmail.com>
2024-12-10 15:50:10 +0530

    all section created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll'
 import contactImg from '../../assets/contact.png'
+import menu from '../../assets/menu.png'
 
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
 <nav className="navbar">
     <img src={logo} alt="logo" className='logo' />
     <div className="desktopMenu">
-    <Link activeClass='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
+    <Link activeClass='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className='dsktopMenuListItem'>Home</Link>
     <Link activeClass='active' to ='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>About</Link>
     <Link activeClass='active' to ='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Portfolio</Link> 
     <Link activeClass='active' to ='clients' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Clients</Link> 
@@ -23,6 +25,19 @@ const Navbar = () => {
         <img className='desktopMenuImg' src={contactImg} alt="contactImg" />
         Contact Me
     </button>
+
+    <img src={menu} alt="logo" className='mobmenu' onClick={()=>setShowMenu(!showMenu)} />
+    <div className="navMenu" style={{display:showMenu ? 'flex':'none'}}>
+    <Link activeClass='active' to ='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem' >Home</Link>
+    <Link activeClass='active' to ='skills' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>About</Link>
+    <Link activeClass='active' to ='works' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>Portfolio</Link> 
+    <Link activeClass='active' to ='clients' spy={true} smooth={true} offset={-50} duration={500} className='ListItem' >Clients</Link> 
+    <Link activeClass='active' to ='contact' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>Contact</Link> 
+
+
+
+ 
+    </div>
 
 </nav>
   )
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,56 +1,86 @@
-import React from 'react'
-import './contact.css'
-import walmart from '../../assets/walmart.png'
-import Adobe from '../../assets/adobe.png'
-import Microsoft from '../../assets/microsoft.png'
-import Facebook from '../../assets/facebook.png'
-import Facebookicon from '../../assets/facebook-icon.png'
-import Twittericon from '../../assets/twitter.png'
-import youtubeIcon from '../../assets/youtube.png'
-import InstagramIcon from '../../assets/instagram.png'
-
-
-
-
+import React, { useRef } from "react";
+import "./contact.css";
+import walmart from "../../assets/walmart.png";
+import Adobe from "../../assets/adobe.png";
+import Microsoft from "../../assets/microsoft.png";
+import Facebook from "../../assets/facebook.png";
+import Facebookicon from "../../assets/facebook-icon.png";
+import Twittericon from "../../assets/twitter.png";
+import youtubeIcon from "../../assets/youtube.png";
+import InstagramIcon from "../../assets/instagram.png";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_7eg5x6j", "template_ndbilyq", form.current, "dDCb4SoysotyASlve",)
+      .then(
+        (result) => {
+          console.log("SUCCESS!", result.text);
+          alert("Email Send !")
+          e.target.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <section id="contactPage">
-        <div id="clients">
-            <h1 className="contactPageTitle">My Clients</h1>
-<p className="clientDesc">
-    I have had the opportunity to work with a diverse group of companies.some of the notable companies i have worked with includes
-</p>
-<div className="clientimgs">
-    <img src={walmart} alt="" className="clientimg" />
-    <img src={Adobe} alt="" className="clientimg" />
-    <img src={Microsoft} alt="" className="clientimg" />
-    <img src={Facebook} alt="" className="clientimg" />
-
-</div>
+      <div id="clients">
+        <h1 className="contactPageTitle">My Clients</h1>
+        <p className="clientDesc">
+          I have had the opportunity to work with a diverse group of
+          companies.some of the notable companies i have worked with includes
+        </p>
+        <div className="clientimgs">
+          <img src={walmart} alt="" className="clientimg" />
+          <img src={Adobe} alt="" className="clientimg" />
+          <img src={Microsoft} alt="" className="clientimg" />
+          <img src={Facebook} alt="" className="clientimg" />
         </div>
-        <div id="contact">
-<h1 className="contactpageTitle">Contact Me</h1>
-<span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
-   <form action="" className="contactform">
-    <input type="text" className="name" placeholder='Your name'/>
-    <input type="email" className="email" placeholder='Your Email' />
-    <textarea name="message" rows="5 "  className="msg" placeholder='Your message'></textarea>
-   <button className="submitbtn" type='submit' value='send'>Submit</button>
-   <div className="links">
-        <img src={Facebookicon} alt="" className="link" />
-        <img src={Twittericon} alt="" className="link" />
-        <img src={youtubeIcon} alt="" className="link" />
-        <img src={InstagramIcon} alt="" className="link" />
-        
-       
-    </div>
-    </form> 
-        </div>
-        
-       
+      </div>
+      <div id="contact">
+        <h1 className="contactpageTitle">Contact Me</h1>
+        <span className="contactDesc">
+          Please fill out the form below to discuss any work opportunities
+        </span>
+        <form className="contactform" ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            className="name"
+            placeholder="Your name"
+            name="your_name"
+          />
+          <input
+            type="email"
+            className="email"
+            placeholder="Your Email"
+            name="your_email"
+          />
+          <textarea
+            name="message"
+            rows="5 "
+            className="msg"
+            placeholder="Your message"
+          ></textarea>
+          <button className="submitbtn" type="submit" value="send">
+            Submit
+          </button>
+          <div className="links">
+            <img src={Facebookicon} alt="" className="link" />
+            <img src={Twittericon} alt="" className="link" />
+            <img src={youtubeIcon} alt="" className="link" />
+            <img src={InstagramIcon} alt="" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
